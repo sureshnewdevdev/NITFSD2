6d08e5ca418c932cf77fde166e09ff26a0b0b7ce
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-10-03 08:05:57 +0530

    Delegate example

---
+++ ExampleOfDelegateClass1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NITFSD2
+{
+    public class ExampleOfDelegateClass1
+    {
+        public delegate int MyDelegateFunction(int first, int second);
+        public void DoWork()
+        {
+            MyDelegateFunction myFunction = new MyDelegateFunction(Add);
+
+            var ans = myFunction(10, 90);
+
+            Console.WriteLine("Answer = " + ans);
+
+        }
+
+        private int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,42 +1,81 @@
-﻿namespace NITFSD2
+﻿using System.ComponentModel;
+
+namespace NITFSD2
 {
     internal class Program
     {
+
+        public delegate int MyDelegateFunction(int first, int second);
+        public static void DoWork()
+        {
+            MyDelegateFunction myFunction = new MyDelegateFunction(Add);
+
+            var ans = myFunction(10, 90);
+
+            MyDelegateFunction myFuntions=new MyDelegateFunction(Add);
+
+            myFuntions += Subtract;
+            myFuntions += Multiply;
+
+            var result = myFuntions.Invoke(100,90);
+
+            Console.WriteLine("Answer = " + ans);
+            Console.WriteLine("Answer = " + result);
+
+        }
+
+        private static int Multiply(int first, int second)
+        {
+            return first + second;
+        }
+        private static int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        private static int Subtract(int first, int second)
+        {
+            return first + second;
+        }
         static void Main(string[] args)
         {
-            int choice = 0;
-            int div = 0;
-            int n = 0;
-            while (choice != -1)
-            {
-                try
-                {
-                    Console.WriteLine("Enter a divider");
-                    div = int.Parse(Console.ReadLine());
+            DoWork();
 
-                    Console.WriteLine("Enter a Main number");
-                    n = int.Parse(Console.ReadLine());
-                    int result = Calculate(div, n);
-                    Console.WriteLine(result);
-                }
-                catch (System.DivideByZeroException ex) {
-                    Console.WriteLine("Divider is zero this input can't be given as zero");
-                }
-                catch(MyOwnException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Internal error " );
-                }
-                finally
-                {
-                    div = 0; n = 0;
-                }
-                Console.WriteLine("Please enter your choice if choice is -1 the exit, else continue");
-                choice = int.Parse(Console.ReadLine());
-            }
+            #region Exception Handling
+            //int choice = 0;
+            //int div = 0;
+            //int n = 0;
+            //while (choice != -1)
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("Enter a divider");
+            //        div = int.Parse(Console.ReadLine());
+
+            //        Console.WriteLine("Enter a Main number");
+            //        n = int.Parse(Console.ReadLine());
+            //        int result = Calculate(div, n);
+            //        Console.WriteLine(result);
+            //    }
+            //    catch (System.DivideByZeroException ex) {
+            //        Console.WriteLine("Divider is zero this input can't be given as zero");
+            //    }
+            //    catch(MyOwnException ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //    catch (Exception ex)
+            //    { 
+            //        Console.WriteLine("Internal error " );
+            //    }
+            //    finally
+            //    {
+            //        div = 0; n = 0;
+            //    }
+            //    Console.WriteLine("Please enter your choice if choice is -1 the exit, else continue");
+            //    choice = int.Parse(Console.ReadLine());
+            //}
+            #endregion
 
 
         }
